a9bdecfcd4766b1ee93f2863218f16eafd8e4df8
shaozeming <szm19920426@gmail.com>
2017-08-16 11:28:09 +0800

    edit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,43 +24,34 @@ class HomeController extends Controller
     public function index()
     {
 
-        echo$v= base64_encode('今天其实想和聊天开视频的，可是感觉昨天聊了那么久，今天再找你，你会觉得烦，就不敢了。今天是我们qq好友177天,我们认识了198天了。');
+//        echo$v= base64_encode('每次给你发了微信后，你没有回我，我就像失去了整个世界，每次一听到微信的声音，我就立马激动的打开，失落的关上，我不知道这样会不会然你产生厌烦，我只知道，如果有好感就是喜欢，如果这种好感经得起考验，那就是爱。今天q好友185天,认识了206天。');
+        echo$v= base64_encode('等哪天，我不喜欢你了，我就会来你空间把这些留言全部删除掉。如果你没有删除我的，我就会一直喜欢你。');
         echo '<br>';
-        echo $vs= $v &(md5('ming'));
-        echo '<br>';
-        echo base64_decode('5LuK5aSp5YW25a6e5oOz5ZKM6IGK5aSp5byA6KeG6aKR55qE77yM5Y+v5piv5oSf6KeJ5pio5aSp6IGK5LqG6YKj5LmI5LmF77yM5LuK5aSp5YaN5om+5L2g77yM5L2g5Lya6KeJ5b6X54Om77yM5bCx5LiN5pWi5LqG44CC5LuK5aSp5piv5oiR5LuscXHlpb3lj4sxNzflpKks5oiR5Lus6K6k6K+G5LqGMTk45aSp5LqG44CC');
-        echo '<br>';
-        echo base64_decode($vs);
-        echo '<br>';
-//var_dump(md5('240610708') == md5('QNKCDZO'));
-//var_dump(md5('aabg7XSs') == md5('aabC9RqS'));
-//var_dump(sha1('aaroZmOk') == sha1('aaK1STfY'));
-//var_dump(sha1('aaO8zKZF') == sha1('aa3OFF9m'));
-//var_dump('0010e2' == '1e3');
-//var_dump('0x1234Ab' == '1193131');
-//var_dump('0xABCdef' == ' 0xABCdef');
-//var_dump(0 == 'abcdefg');
-//var_dump(1 == '1abcdef');
-//        echo base64_decode('SUBnaXVlbS5jb20=');
+        echo base64_decode('5LuK5aSp5L2g5bGF54S257uZ5oiR5Y+R56yR6K+d77yM5oiR5aW95byA5b+D44CC5oiR5LiA5aSp6YO95Zyo56yR77yM5Zug5Li65L2g6IO95YiG5Lqr5b+r5LmQ57uZ5oiR44CCcHM65L2g55qE56yR6K+d5LiN5aW956yR77yM5LiA55yL5L2g5bCx56yR54K55L2O77yM5LqM5aSp5oiR57uZ5L2g5Y+R56yR6K+d5LiN6IO95Y+R56yR54K56auY55qE5LqG77yM5a6z5oCV5L2g55yL5LiN5oeC77yM5oiW6ICF5Yiw5pe25YCZ56yR5YK75LqG5oCO5LmI5Yqe77yM5ZWm5ZWm5ZWm5ZWm5ZWm77yB44CC5ZOI5ZOI77yM6L+Y5pyJ77yM5LuK5aSp5oiR5a+55L2g5LiL5LqG5aWX77yM5LiN5ZGK6K+J5L2g5piv5LuA5LmI5aWXXCheb14pL37jgILku4rlpKlx5aW95Y+LMTgy5aSpLOiupOivhuS6hjIwM+WkqeS6huOAgg==
+');
 //        return view('home');
     }
 
 
-    public function demo()
+    public function indexs()
     {
 
+//        dd(explode(',','asa,ss,dd,ff,gg,hh,jj'));
 //代码作者：giuem
 //更多源码请访问www.giuem.com
         $qq = "928240096";
-        $sid = "";
+//        $sid = "AQVb5Hb8RaZygg7ZucuW5XGZ";
+        $sid = "cdXqOisfZfPoxYwWsoDonNdtfwEjZeTD3753d1e00201%3D%3D";
 
-        $url = "http://ish.z.qq.com/infocenter_v2.jsp?B_UID={$qq}&sid={$sid}&g_ut=1";
-        $content = url_get($url);
-        $zz = getmiddltxt($content, ']<a href="http://blog', '">赞');
+//        $url = "http://ish.z.qq.com/infocenter_v2.jsp?B_UID={$qq}&sid={$sid}&g_ut=1";
+        $url = "https://h5.qzone.qq.com/mqzone/index";
+        $content = $this->url_get($url);
+        $zz = $this->getmiddltxt($content, ']<a href="http://blog', '">赞');
         $a = count($zz);
+        var_dump($content);
         for ($i = 1; $i <= $a; $i++) {
             $url1 = $zz[$i - 1][0];
-            $url2 = getmiddltxt($url1, ']<a href="', '">赞');
+            $url2 = $this->getmiddltxt($url1, ']<a href="', '">赞');
             file_get_contents(html_entity_decode($url2[0][1]));
             echo "成功";
             sleep(5);
