9cac4b49be34c05cdbc2cbedf629df92c9422425
shaozeming <szm19920426@gmail.com>
2017-11-23 16:00:06 +0800

    test

--- app/Http/Controllers/V1/WeixinController.php
+++ app/Http/Controllers/V1/WeixinController.php
@@ -173,11 +173,37 @@ class WeixinController extends Controller
         Log::info('获取请求数据', [$request, __METHOD__]);
 
         $app = app('wechat');
+        $msgArr = $app->getMessage();
+        Log::info('请求message',$msgArr);
+
         $app->server->setMessageHandler(function ($message) {
-            Log::info('请求message',[$message]);
-//             $message->FromUserName; // 用户的 openid
-//             $message->MsgType; // 消息类型：event, text....
-            return "您好！欢迎关注我!";
+          switch ($message->MsgType) {
+        case 'event':
+            return '收到事件消息';
+            break;
+        case 'text':
+            return '收到文字消息';
+            break;
+        case 'image':
+            return '收到图片消息';
+            break;
+        case 'voice':
+            return '收到语音消息';
+            break;
+        case 'video':
+            return '收到视频消息';
+            break;
+        case 'location':
+            return '收到坐标消息';
+            break;
+        case 'link':
+            return '收到链接消息';
+            break;
+        // ... 其它消息
+        default:
+            return '收到其它消息';
+            break;
+    }
         });
 
         return $app->server->serve();
